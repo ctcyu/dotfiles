c094aeaa7f485b2aef8df3837585951e0524243e
Christine Yu <christine.tc.yu@gmail.com>
2013-09-29 16:24:17 -0700

    more zsh config

--- aliases.zsh
+++ aliases.zsh
@@ -4,4 +4,4 @@ alias ..='cd ..'
 alias win='cd /mnt/sda2/'
 alias windocs='cd /mnt/win1/Documents\ and\ Settings/Christine/Documents'
 alias cfxstart='cd ~/addon-dev/addon-sdk-1.10; source bin/activate ; cd -'
-
+alias ll='ls -GFhl'

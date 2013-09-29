e4f82087f1ddd97f0563e6d092c7444c4e17f9b3
Christine Yu <christine.tc.yu@gmail.com>
2013-09-29 16:42:50 -0700

    more zsh config thanks http://zanshin.net/2013/02/02/zsh-configuration-from-the-ground-up/

--- functions.zsh
+++ functions.zsh
@@ -55,3 +55,31 @@ function sc {
     script/console $@
   fi
 }
+
+# -------------------------------------------------------------------
+# display a neatly formatted path
+# -------------------------------------------------------------------
+path() {
+echo $PATH | tr ":" "\n" | \
+awk "{ sub(\"/usr\", \"$fg_no_bold[green]/usr$reset_color\"); \
+sub(\"/bin\", \"$fg_no_bold[blue]/bin$reset_color\"); \
+sub(\"/opt\", \"$fg_no_bold[cyan]/opt$reset_color\"); \
+sub(\"/sbin\", \"$fg_no_bold[magenta]/sbin$reset_color\"); \
+sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
+print }"
+}
+
+# -------------------------------------------------------------------
+# any function from http://onethingwell.org/post/14669173541/any
+# search for running processes
+# ------------------------------------------------------------------- 
+any() {
+    emulate -L zsh
+    unsetopt KSH_ARRAYS
+    if [[ -z "$1" ]] ; then
+        echo "any - grep for process(es) by keyword" >&2
+        echo "Usage: any " >&2 ; return 1
+    else
+        ps xauwww | grep -i --color=auto "[${1[1]}]${1[2,-1]}"
+    fi
+}
